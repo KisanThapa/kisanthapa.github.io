e8ca8a464ccdfac61bcf68b0df5c35deb44407c9
Kisan Thapa <kisanthapa33@gmail.com>
2024-12-19 12:51:54 -0500

    Some features added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    darkMode: 'class', // Enables dark mode with a `class` strategy
     content: ['./*.html', './*.js'], // Specify the HTML files to scan for classes
     theme: {
         extend: {
