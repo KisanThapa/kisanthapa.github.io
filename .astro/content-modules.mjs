ef03150d02db6534fe13e61347be0a5eba1d1990
Kisan Thapa <kisanthapa33@gmail.com>
2026-03-12 23:19:38 -0400

    better design

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/graphsage.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgraphsage.mdx&astroContentModuleFlag=true")],
+["src/content/blog/precision_recall.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprecision_recall.mdx&astroContentModuleFlag=true")]]);
+		
---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwindcss from '@tailwindcss/vite'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
+export default defineConfig({
+  site: 'https://kisanthapa.github.io',
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+})
